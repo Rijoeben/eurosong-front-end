3860f23ccdde55f27f4580391e6028f816414199
Rijoeben <61225406+Rijoeben@users.noreply.github.com>
2021-11-22 10:43:11 +0100

    push

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Eurosong"/>
+    <nav class="c-nav">
+      <ul class="c-nav-list">
+        <li class="c-nav-list-item">Home</li>
+        <li class="c-nav-list-item">Contact</li>
+      </ul>
+    </nav>
+    <h1>Euroson Festival</h1>
+    <div class="c-feedback warning">
+      Er is een fout gebeurd
+    </div>
+    <div class="c-feedback error">
+      Tis kapot
+    </div>
+    <div class="c-feedback succes">
+      joepie het werkt
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//Belangrijk om de juiste file te importen anders werkt het niet 
+import style from './scss/style.scss';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
