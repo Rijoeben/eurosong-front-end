93f6bfb0fdf484674603fe0e685fee2b7eec4f6a
Rijoeben <61225406+Rijoeben@users.noreply.github.com>
2021-12-25 17:25:27 +0100

    final final

--- src/components/Ranking.vue
+++ src/components/Ranking.vue
@@ -1,26 +1,35 @@
 <template>
   <div>
-    <div class="ranking" @mouseover="onhover = false" @mouseleave="onhover=true">  <div :class="{songActive: !onhover}" >{{index +1 }}</div> <div class="result" :class="{songActive: onhover}">{{ song.results }} </div></div>
-         <!-- <div>{{ song.title }}</div>
-          <div>{{ song.artist.name }}</div> -->
-          <div><iframe
-          :src="song.spotify"
-          width="100%"
-          height="75px"
-          frameBorder="0" 
-          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-        ></iframe></div>
+    <div
+      class="ranking"
+      @mouseover="onhover = false"
+      @mouseleave="onhover = true"
+    >
+      <div :class="{ songActive: !onhover }">{{ index + 1 }}</div>
+      <div class="result" :class="{ songActive: onhover }">
+        {{ song.results }}
+      </div>
+    </div>
+    <div>
+      <iframe
+        :src="song.spotify"
+        width="100%"
+        height="75px"
+        frameBorder="0"
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+      ></iframe>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Ranking",
-  props: ["song","index"],
-  data(){
-      return{
-          onhover: true
-      }
-  }
+  props: ["song", "index"],
+  data() {
+    return {
+      onhover: true,
+    };
+  },
 };
 </script>
